2be3a0d0fc9962637c76fd1a83435c132ae5e936
Ryan <123142178+MrRyanPerson@users.noreply.github.com>
2026-08-23 00:34:51 +0000

    Ux improvements

--- src/lib/components/preview.svelte
+++ src/lib/components/preview.svelte
@@ -13,7 +13,7 @@
 </script>
 
 <div class="p-4 rounded-box shadow-sm bg-base-200">
-    <h1 class="text-xl sm:text-2xl font-bold w-full">Generate Preview</h1>
+    <h1 class="text-xl sm:text-2xl font-bold w-full">Preview</h1>
     <button onclick={()=>{
             loadPreview();
             (document.getElementById('preview_modal') as HTMLDialogElement)?.showModal();
